b94179f844ef6cb729fb2596de179d4daf360573
Reuben Allotey <reubenallotey434@gmail.com>
2024-11-01 11:27:57 +0000

    javascript fundamentals 1

---
+++ script.js
@@ -0,0 +1,36 @@
+// variables and values 
+let firstName = "jonas";
+console.log(firstName);
+// datatypes
+// number:let age =34;
+// string:let lastName='Reuben ';
+// Boolean: let fullAge=true;
+
+/*let, const  ,var
+    let is used when the variable can change 
+the const is used when the variable will not changed  and when using we also need an  initializer.
+var is also used to declare variables that  can be change
+*/
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2**3 means 2 raise to the power 3 = 2*2*2
+
+let x = 10 + 5; //15
+x += 10;//x= x + 10=25
+console.log(x);
+
+
+
+
+
+
+
+
+
+
