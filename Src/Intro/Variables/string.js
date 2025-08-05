2a30b5d8ded91e96b61cf075b89515785e594e01
Reuben Allotey <reubenallotey434@gmail.com>
2025-08-05 15:19:03 +0000

    all about strings in javascript

---
+++ Src/Intro/Variables/string.js
@@ -0,0 +1,28 @@
+// concatenating strings
+
+var myName = " Reuben ";
+let age = 23;
+
+console.log(" my name is " + myName + " I'm " + age + "years ");
+
+// string length and retrieving ( in your browser)
+
+// var tweet = prompt("compose your tweet");
+// console.log(
+//   " you have written " +
+//     tweet.length +
+//     " characters , " +
+//     " you have " +
+//     (140 - tweet.length) +
+//     "left"
+// );
+
+// slice ( variableName.slice ( x,y))
+
+var name = " Reuben";
+name.slice(2, 3);
+
+// changing to upper and lower case
+var firstName = " angela";
+firstName = firstName.toLowerCase;
+firstName = firstName.toUpperCase;
