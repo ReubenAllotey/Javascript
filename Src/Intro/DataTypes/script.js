a7542cedb673fdc4728ae2169e70a4b2d60d18c7
Reuben Allotey <reubenallotey434@gmail.com>
2025-08-05 14:05:30 +0000

    javascript datatypes

---
+++ Src/Intro/DataTypes/script.js
@@ -0,0 +1,11 @@
+// datatypes
+ number:let age =34;
+string:let lastName='Reuben ';
+Boolean: let fullAge=true;
+
+/*  
+string : is a set of characters 
+number : is a special character so no need to add quotation marks 
+Boolean: is the true or false  value 
+
+*/
