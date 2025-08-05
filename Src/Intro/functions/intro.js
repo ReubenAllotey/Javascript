1effc5fbca29aae7d3ca87f5a9e57d118dd4cfe2
Reuben Allotey <reubenallotey434@gmail.com>
2025-08-05 23:03:14 +0000

    function in javascript

---
+++ Src/Intro/functions/intro.js
@@ -0,0 +1,22 @@
+/* function  name () {} -> create the function 
+  getname (); */ // calling the function
+
+function getMilk() {
+  console.log("move right");
+  console.log("move right");
+  console.log("move up ");
+  console.log("move up ");
+  console.log("move up ");
+  console.log("move up ");
+  console.log("get in ");
+  console.log("buy milk  ");
+  console.log("get out ");
+  console.log(" move down");
+  console.log("move down  ");
+  console.log("move down  ");
+  console.log("move down  ");
+  console.log("move left  ");
+  console.log("move left  ");
+}
+
+getMilk();
