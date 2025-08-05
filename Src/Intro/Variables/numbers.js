3f37fd8bd2c8e14a87080e6491821f3296dba0ae
Reuben Allotey <reubenallotey434@gmail.com>
2025-08-05 22:27:42 +0000

    numbers in javascript

---
+++ Src/Intro/Variables/numbers.js
@@ -0,0 +1,16 @@
+let a = 3 + 7; // addition
+var d = 3 * 7; // multiplication
+var e = 7 - 3; // subtraction
+var p = 9 % 6; // modulo
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2**3 means 2 raise to the power 3 = 2*2*2
+
+let x = 10 + 5; //15
+x += 10; //x= x + 10=25
+console.log(x);
