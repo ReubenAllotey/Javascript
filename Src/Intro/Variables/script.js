e8639d9b0e32de06b432d31527512e8be303074f
Reuben Allotey <reubenallotey434@gmail.com>
2025-08-05 14:37:03 +0000

    Naming Javscript variables

---
+++ Src/Intro/Variables/script.js
@@ -0,0 +1,14 @@
+// variables and values
+let firstName = "jonas";
+console.log(firstName);
+
+var myName = " Reuben ";
+let age = 23;
+
+console.log(" my name is " + myName + " I'm " + age + "years ");
+
+/*let, const  ,var
+    let is used when the variable can change 
+the const is used when the variable will not changed  and when using we also need an  initializer.
+var is also used to declare variables that  can be change
+*/
