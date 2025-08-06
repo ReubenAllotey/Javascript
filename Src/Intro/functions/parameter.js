84aad98f7fe2d0591016b3f383e7a4bd5dd3ef70
Reuben Allotey <reubenallotey434@gmail.com>
2025-08-06 20:29:30 +0000

    adding parameter to fuctions

---
+++ Src/Intro/functions/parameter.js
@@ -0,0 +1,29 @@
+/* parameter and argument
+
+function name (parameter ){
+
+argument
+
+}
+
+*/
+
+function getMilk(bottles) {
+  console.log("move right");
+  console.log("move right");
+  console.log("move up ");
+  console.log("move up ");
+  console.log("move up ");
+  console.log("move up ");
+  console.log("get in ");
+  console.log("buy" + bottles + "milk  ");
+  console.log("get out ");
+  console.log(" move down");
+  console.log("move down  ");
+  console.log("move down  ");
+  console.log("move down  ");
+  console.log("move left  ");
+  console.log("move left  ");
+}
+
+getMilk(7);
