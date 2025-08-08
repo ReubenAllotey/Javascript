544dfc5939eaf0733bb422f543d35d1709321f22
Reuben Allotey <reubenallotey434@gmail.com>
2025-08-08 22:01:38 +0000

    document object model

---
+++ Src/DOM/index.js
@@ -0,0 +1,5 @@
+document.getElementById("title").innerHTML = "goodbye";
+document.getElementsByTagName("li")[2].style.color = "red";
+document.querySelector(".btn").style.color = " blue";
+document.querySelector("li a").style.color = "green";
+document.querySelectorAll("#list .item")[1].style.color = "yellow";
